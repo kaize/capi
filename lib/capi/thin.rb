45faa02a5ec04daae1d2fd87035042e69a9ab226
Rozhnov Alexandr <nox73@ya.ru>
2012-06-26 02:59:22 +0400

    thin start/stop

---
+++ lib/capi/thin.rb
@@ -0,0 +1,22 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :thin do
+
+    desc 'thin start'
+    task :start do
+      run "cd #{deploy_to}/current && bundle exec thin -c #{deploy_to}/current/config/thin.yml -e #{rails_env} -d"
+    end
+
+    desc 'thin stop'
+    task :stop  do
+      run "kill -QUIT `cat #{deploy_to}/shared/pids/thin.pid`"
+    end
+
+    desc 'thin restart'
+    task :restart do
+      stop rescue nil
+      start
+    end
+
+
+  end
+end
