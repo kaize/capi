2cfd65b636f33a0ca925846b414b380d6f14958c
Rozhnov Alexandr <nox73@ya.ru>
2012-06-26 03:52:16 +0400

    thin

--- lib/capi/thin.rb
+++ lib/capi/thin.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     desc 'thin start'
     task :start do
-      run "cd #{deploy_to}/current && bundle exec thin start -C #{deploy_to}/current/config/thin.yml -P #{deploy_to}/shared/pids/thin.pid -e #{rails_env} -d"
+      run "cd #{deploy_to}/current && bundle exec thin start -S #{deploy_to}/shared/.sock -C #{deploy_to}/current/config/thin.yml -P #{deploy_to}/shared/pids/thin.pid -e #{rails_env} -d"
     end
 
     desc 'thin stop'
