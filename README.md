e4ecb2b3b08888cf45c872df2072dd08da581040
Mikhail Stolbov <mstolbov@gmail.com>
2012-10-18 15:38:36 +0400

    updated readme

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ For example.
 Add this to you `config/deploy.rb` to restart unicorn after deploy:
     
     after "deploy:restart", "unicorn:restart"
+
+To invoke same rake task on remote server execute follow command
+`cap capi:task_invoke`. Example:
+
+    cap capi:task_invoke TASK="db:seed"
--- lib/capi/capi.rb
+++ lib/capi/capi.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     desc 'invoke rake task. Example: cap capi:task_invoke TASK="db:seed"'
     task :task_invoke  do
-      run "cd #{deploy_to}/current && bundle exec rake #{ENV['TASK']} RAILS_ENV=#{rails_env}"
+      run "cd #{deploy_to}/current && RAILS_ENV=#{rails_env} bundle exec rake #{ENV['TASK']}"
     end
 
   end
