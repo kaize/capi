90371fddd97716035347f3aee5fc55650d5e51e9
Alexey Astafyev <av.astafyev@gmail.com>
2013-07-10 02:07:20 +0400

    updated custom config_path & pid_file

--- lib/capi/unicorn.rb
+++ lib/capi/unicorn.rb
@@ -2,14 +2,14 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :unicorn do
     desc 'unicorn start'
     task :start do
-      unicorn_config ||= "#{deploy_to}/current/config/unicorn.rb"
-      run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
+      config_path = unicorn_config || "#{deploy_to}/current/config/unicorn.rb"
+      run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{config_path} -E #{rails_env} -D"
     end
 
     desc 'unicorn stop'
     task :stop do
-      unicorn_pid_file ||= "#{deploy_to}/shared/pids/unicorn.pid"
-      run "kill -QUIT `cat #{unicorn_pid_file}`"
+      pid_file = unicorn_pid_file || "#{deploy_to}/shared/pids/unicorn.pid"
+      run "kill -QUIT `cat #{pid_file}`"
     end
 
     desc 'unicorn restart'
