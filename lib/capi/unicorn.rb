2f116aa43c4765d8c02d5472f6deb6dbe5f33858
Alexey Astafyev <av.astafyev@gmail.com>
2013-07-09 22:41:49 +0400

    added support of custom paths for unicorn config & pid-file

--- lib/capi/unicorn.rb
+++ lib/capi/unicorn.rb
@@ -2,12 +2,14 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :unicorn do
     desc 'unicorn start'
     task :start do
-      run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{deploy_to}/current/config/unicorn.rb -E #{rails_env} -D"
+      unicorn_config ||= "#{deploy_to}/current/config/unicorn.rb"
+      run "cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
     end
 
     desc 'unicorn stop'
     task :stop do
-      run "kill -QUIT `cat #{deploy_to}/shared/pids/unicorn.pid`"
+      unicorn_pid_file ||= "#{deploy_to}/shared/pids/unicorn.pid"
+      run "kill -QUIT `cat #{unicorn_pid_file}`"
     end
 
     desc 'unicorn restart'
